63616bbabf978e7bff2beb92fdbe3ff6702c85f9
Vlad <xergut@gmail.com>
2020-07-14 22:31:26 +0300

    some changes

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+
+const App = () => {
+    return (  
+        <div>
+            <HookSwitcher />
+        </div>
+    );
+}
+ 
+
+
+const HookSwitcher =  () => {
+    const [ color, setColor ] = useState('black')
+
+    return (
+        <div style={{padding: '10px', backgroundColor: color}}>
+            <button onClick={() => {setColor('black')}}>Dark</button>
+            <button onClick={() => {setColor('white')}}>Light</button>
+        </div>
+    )
+}
+ReactDOM.render(<App />, document.getElementById("root"));
